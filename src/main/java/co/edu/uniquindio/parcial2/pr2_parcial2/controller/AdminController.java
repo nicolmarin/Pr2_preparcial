82669a8670418f2602a21210c9832eb7f33c2304
AnduellgDeveloper <anduellgames@gmail.com>
2024-10-11 14:21:06 -0500

    Cambios

--- src/main/java/co/edu/uniquindio/parcial2/pr2_parcial2/controller/AdminController.java
+++ src/main/java/co/edu/uniquindio/parcial2/pr2_parcial2/controller/AdminController.java
@@ -1,4 +1,10 @@
 package co.edu.uniquindio.parcial2.pr2_parcial2.controller;
 
+import co.edu.uniquindio.parcial2.pr2_parcial2.factory.ModelFactory;
+
 public class AdminController {
+    ModelFactory modelFactory;
+    public AdminController(){
+        modelFactory = ModelFactory.getInstancia();
+    }
 }
--- src/main/java/co/edu/uniquindio/parcial2/pr2_parcial2/viewcontroller/AdminViewController.java
+++ src/main/java/co/edu/uniquindio/parcial2/pr2_parcial2/viewcontroller/AdminViewController.java
@@ -1,35 +1,13 @@
 package co.edu.uniquindio.parcial2.pr2_parcial2.viewcontroller;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class AdminViewController {
-
-    @FXML
-    private TextField txtCantidadClientesMayorPrestamos;
-
-    @FXML
-    private TextField txtRangoCMayorPrestamos2;
-
-    @FXML
-    private Button btnConsultarObjetosMayorPrestamos;
-
-    @FXML
-    private Button btnConsultarObjetosID;
-
-    @FXML
-    private TextField txtRangoObjetoXID;
-
-    @FXML
-    private TableColumn<?, ?> tabObjetosDND;
-
-    @FXML
-    private TextField txtCantidadObjetosMPrestamos;
-
-    @FXML
-    private ListView<?> listObjetosMayorPrestamos11;
 
     @FXML
     private Button btnConsultarClMenorPrestamos;
@@ -38,16 +16,16 @@ public class AdminViewController {
     private Button btnConsultarClientesMayorPrestamos2;
 
     @FXML
-    private TextField txtBuscarObjetoID;
+    private Button btnConsultarObjetosID;
 
     @FXML
-    private TextField txtRangoCMenorPrestamos;
+    private Button btnConsultarObjetosMayorPrestamos;
+
+    @FXML
+    private Button btnConsultarObjetosXEstado;
 
     @FXML
     private ListView<?> listCMenorPrestamos;
-
-    @FXML
-    private TextField txtRangoOMayorPrestamos;
 
     @FXML
     private ListView<?> listObjetosMayorPrestamos;
@@ -56,10 +34,34 @@ public class AdminViewController {
     private ListView<?> listObjetosMayorPrestamos1;
 
     @FXML
+    private ListView<?> listObjetosMayorPrestamos11;
+
+    @FXML
+    private TableColumn<?, ?> tabObjetosDND;
+
+    @FXML
+    private TextField txtBuscarObjetoID;
+
+    @FXML
+    private TextField txtCantidadClientesMayorPrestamos;
+
+    @FXML
+    private TextField txtCantidadObjetosMPrestamos;
+
+    @FXML
     private TextField txtCantidadObjetosMPrestamos1;
 
     @FXML
-    private Button btnConsultarObjetosXEstado;
+    private TextField txtRangoCMayorPrestamos2;
+
+    @FXML
+    private TextField txtRangoCMenorPrestamos;
+
+    @FXML
+    private TextField txtRangoOMayorPrestamos;
+
+    @FXML
+    private TextField txtRangoObjetoXID;
 
     @FXML
     void onConsultarCMenorPrestamos(ActionEvent event) {
@@ -67,7 +69,7 @@ public class AdminViewController {
     }
 
     @FXML
-    void onConsultarObjetosMayorPrestamos(ActionEvent event) {
+    void onConsultarClientesMayorPrestamos(ActionEvent event) {
 
     }
 
@@ -77,7 +79,7 @@ public class AdminViewController {
     }
 
     @FXML
-    void onConsultarClientesMayorPrestamos(ActionEvent event) {
+    void onConsultarObjetosMayorPrestamos(ActionEvent event) {
 
     }
 
