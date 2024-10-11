66636b35762ccb26fd3dadbcdcb4fca1d4d87fd3
nicolmarin <nicolmoreno090905@gmail.com>
2024-10-11 12:07:52 -0500

    cambios interfaz y view controller admin

--- src/main/java/co/edu/uniquindio/parcial2/pr2_parcial2/viewcontroller/AdminViewController.java
+++ src/main/java/co/edu/uniquindio/parcial2/pr2_parcial2/viewcontroller/AdminViewController.java
@@ -1,4 +1,87 @@
 package co.edu.uniquindio.parcial2.pr2_parcial2.viewcontroller;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AdminViewController {
+
+    @FXML
+    private TextField txtBuscarXCedula;
+
+    @FXML
+    private TextField txtCantidadObjetosEstado;
+
+    @FXML
+    private MenuButton btnMenuEstado;
+
+    @FXML
+    private ListView<?> listEstadoObjeto;
+
+    @FXML
+    private TextField txtCantidadOMayorDemanda;
+
+    @FXML
+    private Button btnMayorPrestamos;
+
+    @FXML
+    private ListView<?> listMayorPrestamos;
+
+    @FXML
+    private Button btnMayorDemanda;
+
+    @FXML
+    private Button btnObjetosDND;
+
+    @FXML
+    private TextField txtCantidadODND;
+
+    @FXML
+    private TableView<?> tbClientesPrestamos;
+
+    @FXML
+    private Button btnConsultarXCedula;
+
+    @FXML
+    private TextField txtRangoMaximo;
+
+    @FXML
+    private ListView<?> listMayorDemanda;
+
+    @FXML
+    private TextField txtCantidadClientesMayorP;
+
+    @FXML
+    private TableView<?> tbObjetos;
+
+    @FXML
+    void onMayorDemanda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onConsultarXCedula(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMayorPrestamos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onObjetosDND(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMenuEstado(ActionEvent event) {
+
+    }
+
 }
